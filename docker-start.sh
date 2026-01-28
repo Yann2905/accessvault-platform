7c2c63d6ceee36aef903f74fcd935cfb87fc3fb5
Yann2905 <yanncompagnie2@gmail.com>
2026-01-28 09:21:34 +0000

    Fix Apache MPM conflict by removing runtime port config

--- docker-start.sh
+++ docker-start.sh
@@ -58,11 +58,6 @@ php artisan route:cache
 php artisan view:cache
 
 echo ""
-echo "🚀 Démarrage Apache sur le port ${PORT}..."
+echo "🚀 Démarrage Apache..."
 
-# Configurer le port Apache
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/:80/:${PORT}/" /etc/apache2/sites-available/000-default.conf
-
-# Démarrer Apache
-apache2-foreground
+exec apache2-foreground
